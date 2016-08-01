3f9e1db86847cf2cca34c8749dca08ffc262ae41
Armando roque barahona <Armando roque barahona>
2016-07-31 23:56:45 -0600

    camara prefab

---
+++ Assets/iseeyou.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class iseeyou : MonoBehaviour {
+	public GameObject alarm;	
+	protected Animator alarmanim;
+	protected GameObject vision;
+
+	// Use this for initialization
+	void Start () {
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	
+	}
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == "Player") 
+         {
+         	//alarm = this.gameObject.parent.GetChild(1);
+         	alarmanim = alarm.GetComponent<Animator>();
+         	alarmanim.SetInteger("active", 1);
+
+
+         }
+		
+	}
+}
