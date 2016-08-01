590731f8379eed873a193d949e6719e08bcd1364
Jeries Handal <JeriesHG@Gmail.com>
2016-08-01 13:45:31 -0600

    Renaming classes

---
+++ Assets/Scripts/Camara&Alarm/CameraVision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraVision : MonoBehaviour
+{
+	
+	public GameObject alarm;
+	protected Animator alarmAnim;
+	protected GameObject vision;
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.tag == "Player") {
+			alarmAnim = alarm.GetComponent<Animator> ();
+			alarmAnim.SetInteger ("active", 1);
+		}
+	}
+}
