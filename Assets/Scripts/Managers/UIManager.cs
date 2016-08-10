437898e6c992e01f54ee903d9f9aafc8b8d9a8c7
Mike Sabillon <mikesabillon2014@gmail.com>
2016-08-10 15:08:22 -0600

    Fixed Small Issue
    Fixed Github script error on UIManager.

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -7,30 +7,24 @@ public class UIManager : MonoBehaviour
 
 	public static UIManager uiManager;
 
-<<<<<<< HEAD
 	public Canvas UICanvas;
 
-	[Header("Player UI Elements")]
-=======
 	[Header ("Player UI Elements")]
->>>>>>> origin/master
 	public Text scoreText;
 	public Text markerText;
 	public Animator playerControlAnim;
 	public Animator playerInfoAnim;
 	public GameObject playerUICon;
 
-<<<<<<< HEAD
 	[Header("General UI Elements")]
 	public Animator faderAnim;
 	public Animator pauseMenuAnim;
 	public Animator counterAnim;
 	public GameObject pauseUICon;
 	public GameObject counterUICon;
-=======
+
 	[Header ("General UI Elements")]
 	public Animator faderAnimator;
->>>>>>> origin/master
 
 	private GameObject currentPlayerObj;
 	private GameObject currentSceneCam;
@@ -45,7 +39,6 @@ public class UIManager : MonoBehaviour
 		UpdatePlayerInfo ();
 	}
 
-<<<<<<< HEAD
 	public void setPauseMenuState(bool state)
 	{
 		pauseMenuAnim.SetBool ("On",state);
@@ -64,10 +57,7 @@ public class UIManager : MonoBehaviour
 	}
 
 	//Sets the fader on or off depending on state
-	public void setFaderState(bool state)
-=======
 	public void setFaderState (bool state)
->>>>>>> origin/master
 	{
 		faderAnim.SetBool ("FaderOn", state);
 	}
@@ -111,7 +101,6 @@ public class UIManager : MonoBehaviour
 	//Updates player info if player is on scene
 	void UpdatePlayerInfo ()
 	{
-<<<<<<< HEAD
 		if (currentPlayerObj != null)
 		{
 			//Debug.Log ("<color=green><b>PlayerPrefab Detected</b></color>");
@@ -121,23 +110,11 @@ public class UIManager : MonoBehaviour
 		else
 		{
 			//Debug.Log ("<color=red><b>PlayerPrefab Not Found.</b></color>");
-=======
-		if (currentPlayerObj != null) {
-			Debug.Log ("<color=green><b>PlayerPrefab Detected</b></color>");
-			scoreText.text = "Score: " + currentPlayerObj.GetComponent<PlayerController> ().score;
-			markerText.text = "X" + currentPlayerObj.GetComponent<PlayerController> ().getPlayerStats ().markers;
-		} else {
-			Debug.Log ("<color=red><b>PlayerPrefab Not Found.</b></color>");
->>>>>>> origin/master
 		}
 	}
 
 	//Sets the current player obj as reference
-<<<<<<< HEAD
-	public void getUICurrentPlayerObj()
-=======
-	public void setUICurrentPlayerObj (GameObject prefab)
->>>>>>> origin/master
+	public void getUICurrentPlayerObj ()
 	{
 		currentPlayerObj = GameManager.gameManager.currentPlayerObj;
 	}
